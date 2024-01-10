bd319be693270ef88511b973d04b348698e7f5db
revathi <arevathi125@gmail.com>
2024-01-10 17:33:47 -0600

    Changes made

--- errors-and-debugging/chapter-examples/degrees-c-to-k.js
+++ errors-and-debugging/chapter-examples/degrees-c-to-k.js
@@ -1,6 +1,7 @@
 const input = require('readline-sync');
 
-let degreesC = input.question('Temp in degrees C: ');
+let degreesC = Number(input.question('Temp in degrees C: '));
+//console.log(degreesC);
 let degreesK = degreesC + 273.15;
 
 console.log('Degrees K:', degreesK);

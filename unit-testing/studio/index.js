e866bad58195b08e22635ad317baf46c23ab0dab
revathi <arevathi125@gmail.com>
2024-02-07 15:21:46 -0600

    Unit testing excercises done

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,58 @@
 
 let launchcode = {
-  
+    organization : "nonprofit",
+    executiveDirector : "Jeff",
+    percentageCoolEmployees : 100,
+    programsOffered : ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput(){return String; }
 }
 
-module.exports = launchcode;
+function launchOutput(number){
+  
+    if( number % 2 === 0 && number % 3 === 0 && number % 5 === 0){
+        return 'LaunchCode Rocks!';
+    }
 
+    else if( number % 2 !== 0 && number % 3 !== 0 && number % 5 !== 0){
+        return "Rutabagas! That doesn't work.";
+    }
+
+   else if( number % 2 === 0 && number % 3 === 0){
+        return 'LaunchCode!';
+    }
+
+    // else if (number % 2 === 0 && number % 5 === 0){
+    //     return 'Launch Rocks!';
+    // }
+
+    else if (number % 2 === 0 && number % 5 === 0){
+        return 'Launch Rocks! (CRASH!!!!)';
+    }
+
+    else if (number % 3 === 0 && number % 5 === 0){
+        return 'Code Rocks!';
+    }
+
+    else if( number % 2 === 0){
+        return 'Launch!';
+    }
+
+    else if( number % 3 === 0){
+        return 'Code!';
+    }
+
+    else if( number % 5 === 0){
+        return 'Rocks!';
+    }
+ 
+       
+    }
+  
+
+
+
+//Received array: ["Web Development", "Data Analysis", "Liftoff", 3]
+module.exports = {
+    launchcode : launchcode,
+    launchOutput : launchOutput
+}
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -5,4 +5,69 @@ describe("Testing launchcode", function(){
 
   // Write your unit tests here!
   
+  test("returns organization value is 'nonprofit'", () => {
+    expect(launchcode.launchcode.organization).toBe("nonprofit");
+  });
+
+  test("returns executiveDirector value is 'Jeff'", () => {
+    expect(launchcode.launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("returns percentageCoolEmployees value is 100", () => {
+    expect(launchcode.launchcode.percentageCoolEmployees).toBe(100);
+  });
+
+  test("returns programsOffered is ", () => {
+    let programsOffered = ["Web Development", "Data Analysis", "Liftoff"];
+    programsOffered.push(programsOffered.length);
+    expect(programsOffered.length).toBe(4);
+   // expect(programsOffered).toContain(3);
+  });
+
+  test("When passed a number that is ONLY divisible by 2, return 'Launch!'", () => {
+    let result = launchcode.launchOutput(2);
+    expect(result).toBe('Launch!');
+  });
+
+  test("When passed a number that is ONLY divisible by 3, return 'Code!'", () => {
+    let result = launchcode.launchOutput(9);
+    expect(result).toBe('Code!');
+  });
+
+  test("When passed a number that is ONLY divisible by 5, return 'Rocks!'", () => {
+    let result = launchcode.launchOutput(5);
+    expect(result).toBe('Rocks!');
+  });
+
+  test("When passed a number that is divisible by 2 AND 3, return 'LaunchCode!'", () => {
+    let result = launchcode.launchOutput(6);
+    expect(result).toBe('LaunchCode!');
+  });
+
+  test("When passed a number that is divisible by 3 AND 5, return 'Code Rocks!'", () => {
+    let result = launchcode.launchOutput(15);
+    expect(result).toBe('Code Rocks!');
+  });
+
+  // test("When passed a number that is divisible by 2 AND 5, return 'Launch Rocks!'", () => {
+  //   let result = launchcode.launchOutput(10);
+  //   expect(result).toBe('Launch Rocks!');
+  // });
+
+  test("When passed a number that is divisible by 2 AND 5, return 'Launch Rocks! (CRASH!!!!)'", () => {
+    let result = launchcode.launchOutput(10);
+    expect(result).toBe('Launch Rocks! (CRASH!!!!)');
+  });
+
+  test("When passed a number that is divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'", () => {
+    let result = launchcode.launchOutput(30);
+    expect(result).toBe('LaunchCode Rocks!');
+  });
+
+  test("When passed a number that is NOT divisible by 2, 3, AND 5, return 'Rutabagas! That doesn't work.'", () => {
+    let result = launchcode.launchOutput(-1);
+    expect(result).toBe("Rutabagas! That doesn't work.");
+  });
+
+
 });

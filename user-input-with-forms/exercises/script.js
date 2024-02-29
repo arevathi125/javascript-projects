32653146682a077fe33eae718693ec0c9367dc09
revathi <arevathi125@gmail.com>
2024-02-28 21:25:45 -0600

    Http and Forms exercises

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,21 @@
 //Code Your Solution Below
+window.addEventListener("load",function(){
+    // window.alert("Test1");
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        //window.alert("Test1");
+        let testNameInput = document.querySelector("input[name=testName]");
+       //let testNameInput = document.getElementById("input[testName]");
+        console.log(testNameInput.value);
+        let testDateInput = document.querySelector("input[name=testDate]");
+        let rocketTypeInput = document.querySelector("input[name=rocketType]");
+        let boosterCountInput = document.querySelector("input[name=boosterCount]");
+        let windRatingInput = document.querySelector("input[name=windRating]");
+        let productionServersInput = document.querySelector("input[name=productionServers]");
+        if(testNameInput.value === "" || testDateInput.value === ""){
+           alert("All fields required");
+        }
+       // window.alert("test");
+        event.preventDefault();
+    });
+});
